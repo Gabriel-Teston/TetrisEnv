034d299d9944daf81f17d74303d5f2a12e21a1cb
Gabriel Teston <gabriel_teston@hotmail.com>
2019-10-26 09:01:40 -0300

    Fixed setup

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(name='Tetris',
       version='0.0.1',
-      install_requires=['gym', 'numpy']#And any other dependencies required
+      install_requires=['gym', 'numpy'],#And any other dependencies required
+      packages=find_packages()
 )
